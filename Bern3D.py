70c9d4444ba7760df7681c089b86b714a1de5865
thewart <seth.madlonkay@gmail.com>
2024-04-08 14:52:46 -0400

    noisy update rules

--- Bern3D.py
+++ Bern3D.py
@@ -17,18 +17,22 @@ spec = [('sigma', UniTuple(float64,2)),
             ('cue_log_odds', float64),
             ('pvalid', float64),
             ('task_prior', float64),
-            ('sample_actions', UniTuple(ListType(unicode_type), 3)), # must be manually changed for different number of actions :(
+            ('sample_actions', ListType(ListType(unicode_type))),
+            ('update_rule', unicode_type),
+            ('inv_temp', float64),
             ('v', float64[:,:,:])]
 
 @jitclass(spec)
 class Bern3Diff:
-    def __init__(self, sigma=(0.6, 0.6), theta=0.6, pvalid=1.0, task_prior=0.5, cost=-0.001, sample_actions=(List(['x0']), List(['x1']), List(['z'])), state_range=50):
+    def __init__(self, sigma=(0.6, 0.6), theta=0.6, pvalid=1.0, task_prior=0.5, cost=-0.001, \
+                 update_rule='max', inv_temp=1e3, sample_actions=List([List(['x0']), List(['x1']), List(['z'])]), state_range=50):
 
         if cost >= 0:
             raise Exception("You really want cost to be strictly negative.")
 
         self.sigma, self.theta, self.pvalid, self.task_prior = sigma, theta, pvalid, task_prior
         self.cost, self.sample_actions, self.state_range = cost, sample_actions, state_range
+        self.update_rule, self.inv_temp = update_rule, inv_temp
 
         # log lik ratio that evidence x is conistent with stimulus for stim dims 0 and 1
         self.x_log_odds = (log(sigma[0]) - log(1-sigma[0]), log(sigma[1]) - log(1-sigma[1]))      
@@ -37,7 +41,8 @@ class Bern3Diff:
         self.z_log_odds = log(theta) - log(1-theta) 
 
         #prior log odds that task cue indicates task 1, given prior over tasks
-        self.cue_log_odds = log(pvalid*task_prior + (1-pvalid)*(1-task_prior)) - log((1-pvalid)*task_prior + pvalid*(1-task_prior))
+        self.cue_log_odds = log(pvalid*task_prior + (1-pvalid)*(1-task_prior)) - \
+            log((1-pvalid)*task_prior + pvalid*(1-task_prior))
 
         state_len = self.state_range*2 + 1
         self.v = np.empty((state_len, state_len, state_len)) # (x0, x1, z)
@@ -46,24 +51,24 @@ class Bern3Diff:
             x, z = self.state_of_index(ind)
             self.v[ind] = max(self.state_terminate_value_h0(x, z), self.state_terminate_value_h1(x, z))
 
-        # optimize_value(self)
+        optimize_value(self, maxiter=1000)
 
     # core computations 
     @staticmethod
-    def __prob_h(log_odds, x, offset=0.0):
+    def prob_h(log_odds, x, offset=0.0):
         return 1.0 / (1.0 + exp(-(x*log_odds + offset)))
     
     def prob_h1(self, x, task=0):
-        return self.__prob_h(self.x_log_odds[task], x[task])
+        return self.prob_h(self.x_log_odds[task], x[task])
     
     def prob_h0(self, x, task=0):
-        return self.__prob_h(self.x_log_odds[task], -x[task])
+        return self.prob_h(self.x_log_odds[task], -x[task])
     
     def prob_c1(self, z):
-        return self.__prob_h(self.z_log_odds, z, self.cue_log_odds)
+        return self.prob_h(self.z_log_odds, z, self.cue_log_odds)
     
     def prob_c0(self, z):
-        return self.__prob_h(self.z_log_odds, -z, -self.cue_log_odds)
+        return self.prob_h(self.z_log_odds, -z, -self.cue_log_odds)
     
     def next_states(self, x):
         return self.which_state_down(x), self.which_state_up(x)
@@ -118,10 +123,6 @@ class Bern3Diff:
         ind = self.index_of_state(x, z)
         return self.v[ind]
     
-    def state_value_new(self, x, z):
-        tval_h1 = self.state_terminate_value_h1(x, z)
-        return max(self.state_sample_value_max(x, z)[0], 1-tval_h1, tval_h1)
-    
     def state_sample_value_max(self, x, z):
         vstar = -inf
         astar = None
@@ -132,27 +133,30 @@ class Bern3Diff:
                 astar = a
         return vstar, astar
     
-    def state_sample_softmax(self, x, z, inv_temp):
-        v = np.empty(len(self.sample_actions))
+    def state_sample_value_vector(self, x, z):
+        v = np.empty(len(self.sample_actions), float64)
         for ind, a in enumerate(self.sample_actions):
-            v[ind] = inv_temp * self.state_sample_value(x, z, a)
-        v = v - np.max(v)
-        pa = np.exp(v)/np.sum(np.exp(v))
-        aind = np.argmax(np.cumsum(pa) > random())
-        return self.sample_actions[aind]
+            v[ind] = self.state_sample_value(x, z, a)
+        return v
     
-    def state_sample_egreedy(self, x, z, epsilon):
-        if random() < epsilon:
-            nsa = len(self.sample_actions)
-            cumprob = np.linspace(1/nsa, 1, nsa)
-            aind = np.argmax(cumprob > random())
-            return self.sample_actions[aind]
-        else:
-            return self.state_sample_value_max(x, z)[1]
+    def state_action_value_vector(self, x, z):
+        nsa = len(self.sample_actions)
+        v = np.empty(nsa + 2, float64)
+        v_sample = self.state_sample_value_vector(x, z)
+        v[:nsa] = v_sample
+        v[nsa] = self.state_terminate_value_h0(x, z)
+        v[nsa+1] = self.state_terminate_value_h1(x, z)
+        return v
 
     def state_value_update(self, x, z):
         ind = self.index_of_state(x, z)
-        new_value = self.state_value_new(x, z)
+        v = self.state_action_value_vector(x, z)
+        match self.update_rule:
+            case 'max':
+                new_value = np.max(v)
+            case 'softmax':
+                new_value = np.dot(v, softmax_prob(v*self.inv_temp))
+
         value_diff = new_value - self.v[ind]
         self.v[ind] = new_value
         return value_diff
@@ -181,6 +185,15 @@ class Bern3Diff:
             y[ind] = self.in_sample_region(x, z)
         return y
 
+@njit
+def softmax_prob(v):
+    v = v - np.max(v)
+    return np.exp(v)/np.sum(np.exp(v))
+
+@njit
+def choice(p):
+    return np.argmax(np.cumsum(p) > random())
+
 def solution_as_array(diffobj):
     return np.ma.masked_array(diffobj.v, mask=diffobj.get_sample_region())
 
@@ -207,6 +220,7 @@ def update_sweep(diffobj):
         max_change = max(max_change, abs(change))
     return max_change
 
+@njit
 def optimize_value(diffobj, tol=1e-6, maxiter=1e4):
     change = 1
     iter = 0
@@ -217,17 +231,3 @@ def optimize_value(diffobj, tol=1e-6, maxiter=1e4):
         # print(change)
     print(iter)
 
-
-# def view_solution(diffobj):
-#     z = np.ma.masked_array(diffobj.v, mask=diffobj.get_sample_region())
-#     x = np.arange(-diffobj.state_range - .5, diffobj.state_range + 1)
-#     fig, ax = plt.subplots()
-#     ax.pcolormesh(x, x, z)
-#     ax.set_aspect(1)
-#     return fig, ax
-
-# def incongruency_effect(diffobj, niter=int(1e4)):
-#     rt_con, acc_con = diffobj.performance((True, True), niter)
-#     rt_inc, acc_inc = diffobj.performance((True, False), niter)
-     
-#     return rt_con, rt_inc, acc_con, acc_inc
--- DiffSim.py
+++ DiffSim.py
@@ -1,7 +1,8 @@
-from Bern3D import Bern3Diff
-from numba.types import float64, boolean, UniTuple
+from Bern3D import *
+from numba.types import float64, boolean, UniTuple, int32
 from numba.experimental import jitclass
 from random import random
+import numpy as np
 
 
 simspec = [('diffobj', Bern3Diff.class_type.instance_type),
@@ -21,33 +22,48 @@ class DiffSim:
         self.prob_up_x = (diffobj.sigma[0] if h1_status[0] else 1.0 - diffobj.sigma[0], \
                      diffobj.sigma[1] if h1_status[1] else 1.0 - diffobj.sigma[1])
         self.prob_up_z = diffobj.theta if c1_status else 1.0 - diffobj.theta
-        
-    def simulate_agent(self, step_limit=1e4):
-        in_sample_region = True
+            
+    def simulate_agent(self, policy='default', inv_temp=1e3, step_limit=int(1e4)):
+        sampling = True
         steps = 0
         x0 = 0
         x1 = 0
         z = 0
         do = self.diffobj
+        nsa = len(do.sample_actions)
+        if policy == 'default':
+            policy = do.update_rule
+            inv_temp = do.inv_temp
+        
+        path = np.empty((3, step_limit))
+        path[0,0] = x0
+        path[1,0] = x1
+        path[2,0] = z
 
-        while in_sample_region:
-            sample_value, action = do.state_sample_value_max((x0, x1), z)
-            vh0 = do.state_terminate_value_h0((x0, x1), z)
-            vh1 = do.state_terminate_value_h1((x0, x1), z)
-            in_sample_region = sample_value > vh0 and sample_value > vh1
-            # in_sample_region = self.in_sample_region(x, z)
-
-            if in_sample_region:
-                steps += 1
-                if steps > step_limit:
-                    raise Exception('Not all who wander are lost, but this bloke probably is.')
-                else:
-                    x0, x1, z = self.do_sample_action(x0, x1, z, action)
+        while sampling:
+            steps += 1
+            if steps > step_limit:
+                raise Exception('Not all who wander are lost, but this bloke probably is.')
+            
+            v = do.state_action_value_vector((x0, x1), z)
+            match policy:
+                case 'max':
+                    aind = np.argmax(v)
+                case 'softmax':
+                    aind = choice(softmax_prob(inv_temp*v))
+                
+            if aind < nsa:
+                action = do.sample_actions[aind]
+                x0, x1, z = self.do_sample_action(x0, x1, z, action)
+                path[0,steps] = x0
+                path[1,steps] = x1
+                path[2,steps] = z
             else:
-                chose_h1 = vh1 > vh0
+                sampling = False
+                chose_h1 = aind - nsa
                 correct = self.h1_correct == chose_h1
 
-        return steps, correct
+        return steps, correct, path
     
     def do_sample_action(self, x0, x1, z, action):
         do = self.diffobj
@@ -59,11 +75,33 @@ class DiffSim:
             z = do.which_state_up(z) if random() < self.prob_up_z else do.which_state_down(z)
         return x0, x1, z
     
-    def performance(self, niter=int(1e4)):
+    def performance(self, policy='default', inv_temp=1e3, niter=int(1e4), step_limit=int(1e4)):
         rt = 0.
-        acc = 0.
+        acc = 0. 
         for i in range(niter):
-            rti, acci = self.simulate_agent(niter)
+            rti, acci, path = self.simulate_agent(policy, inv_temp, step_limit)
             rt += rti
             acc += acci
         return rt/niter, acc/niter
+
+def performance(obj, policy='default', inv_temp=1e3, niter=int(1e4), step_limit=int(1e4)):
+    return obj.performance(policy, inv_temp, niter, step_limit)
+
+def sample_diff_path(obj, policy='default', inv_temp=1e3, step_limit=int(1e4), only_correct=False, logodds=False, max_step=inf):
+    acceptable = False
+    while not acceptable:
+        steps, correct, path = obj.simulate_agent(policy, inv_temp, step_limit)
+        if ((only_correct and correct) or not only_correct) and steps < max_step:
+            acceptable = True
+    path = np.copy(path[:,:steps])
+    if logodds:
+        newpath = np.empty_like(path)
+        diffobj = obj.diffobj
+        for t in range(steps):
+            x = (path[0,t], path[1,t])
+            newpath[0,t] = diffobj.x_log_odds[0] * path[0,t]
+            newpath[1,t] = diffobj.x_log_odds[1] * path[1,t]
+            newpath[2,t] = diffobj.z_log_odds * path[2,t] + diffobj.cue_log_odds
+        path = newpath
+    return path
+
